41a763d52c47c58b365772d6cb8178839b85416c
Sirui <sirui.qiu01@gmail.com>
2024-08-03 12:49:40 +0000

    refactor: change images

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Portfolio from "./Components/Portfolio";
-import Education from "./Components/Education";
+// import Education from "./Components/Education";
 
 import "./styles.css";
 
@@ -46,7 +46,6 @@ const App = () => {
       <Home name={siteProps.name} title={siteProps.title} />
       <About />
       <Portfolio />
-      <Education />
       <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
     </div>
   );
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/rocket.png";
+import image from "../images/threeBirds.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
